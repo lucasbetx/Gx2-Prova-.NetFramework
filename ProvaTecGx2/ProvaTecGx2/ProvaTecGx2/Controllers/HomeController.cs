ce220d541d85dafe329a686b07104ba6603fc2a2
Lucas B. Teixeira <lucasussx@outlook.com>
2020-07-30 18:40:37 -0300

    Prova Gx2

---
+++ ProvaTecGx2/ProvaTecGx2/ProvaTecGx2/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProvaTecGx2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Questao1()
+        {
+            var j = 0;
+            string msg = "";
+
+            while (j <= 100)
+            {
+
+                if (j % 2 == 0)
+                {
+
+                    if (j == 100)
+                    {
+
+                        msg += j + ". ";
+
+                    }
+                    else
+                    {
+
+                        msg += j + ", ";
+
+                    }
+
+                }
+
+                j++;
+            }
+
+            ViewBag.Numbers = msg;
+
+            return View();
+        }
+
+        public ActionResult Questao2()
+        {
+
+            return View();
+        }
+
+        public ActionResult Questao3()
+        {
+
+            return View();
+        }
+
+        public ActionResult Questao4()
+        {
+            return View();
+        }
+
+        public ActionResult Questao5()
+        {
+
+            return View();
+        }
+
+    }
+}
---
+++ ProvaTecGx2/ProvaTecGx2/ProvaTecGx2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProvaTecGx2.MvcApplication" Language="C#" %>
